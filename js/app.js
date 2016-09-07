7db34dfb4b6372aab6cad38b8f4e33c07767bc01
ashishsajwan <sajwan.ashu@gmail.com>
2016-09-07 21:20:37 +0530

    basic working app

--- js/app.js
+++ js/app.js
@@ -2,23 +2,16 @@
  * This is a backbone model which store
  * the data for a Query/API Entity
  */
-var QueryDataModel = Backbone.Model.extend({
-    defaults: {
-        'stars': {
-            'min': 0,
-            'max': 1000,
-        },
-        'language': 'assembly',
-        'totalRepos': '-',
-        'XRateRemaining': '-',
-        'XRateLimit': '-',
-    }
-});
 /**
  * This is a backbone model which store
  * the data for a Repo Entity
  */
-var RepoModel = Backbone.Model.extend({
+var UserModel = Backbone.Model.extend({
+    urlRoot: 'https://api.github.com/users/',
+    userName: null,
+    url: function() {
+        return this.urlRoot + this.userName;
+    },
     initialize: function() {}
 });
 
@@ -26,13 +19,10 @@ var RepoModel = Backbone.Model.extend({
  * This is a backbone collection which store
  * the list of Repos as collection
  */
-var RepoList = Backbone.Collection.extend({
-    model: RepoModel,
-    parse: function(data) {
-        _.extend(this, _.pick(data, ['total_count', 'incomplete_results']));
-        return data.items;
-    },
-    url: 'https://api.github.com/search/repositories'
+var UserList = Backbone.Collection.extend({
+    model: UserModel,
+    localStorage: new Backbone.LocalStorage('UserList'),
+    initialize: function() {}
 });
 
 /**
@@ -41,183 +31,55 @@ var RepoList = Backbone.Collection.extend({
  */
 var HeaderView = Backbone.View.extend({
     initialize: function() {
-        // listen to update event on model
-        this.listenTo(this.model, 'update', this.render);
         this.render();
     },
     render: function() {
-        var headerTemplate = _.template($('#Tpl-header').html());
-        this.$el.html(headerTemplate(this.model.toJSON()));
+        this.$el.html(_.template($('#Tpl-header').html()));
         $('#app-header-container').html(this.el);
     }
 });
-/**
- * This is a backbone view which is responsible
- * for rendering Repo list and filter options
- */
+
+
 var BodyView = Backbone.View.extend({
     events: {
-        'change input.autocomplete': 'updateSearchData',
-        'click .card-panel': 'goToRepo',
+        'click #add-user-button': 'findAndAddUser'
     },
     initialize: function(data) {
-        /**
-         extend view with data so that custom data which was passed while
-         initializing view can also get attached to view
-        **/
         _.extend(this, data);
-
-        this.renderBody();
-        /*
-          When ever data inside collection is fetched from server 'sync' event
-          will be triggered & then we can render the Repos as list inside this view
-        */
-        this.listenTo(this.collection, 'sync', this.renderReposList);
-        this.listenTo(this.queryData, 'change', this.fetchRepos);
-        this.listenTo(this.queryData, 'update', this.renderFilter);
-        this.fetchRepos();
+        this.listenTo(this.userList, 'sync', this.renderList);
+        this.render();
+        this.userList.fetch();
     },
-    /** update queryData model with new search term
-     *   or new star filter range
-     **/
-    updateSearchData: function(e) {
-        var that = this;
-        if (e.currentTarget) {
-            _.delay(function() {
-                that.queryData.set({
-                    language: $(e.currentTarget).val()
-                });
-            }, 500);
-        } else {
-            this.queryData.set({
-                stars: {
-                    min: e['0'],
-                    max: e['1']
-                }
-            });
-        }
-    },
-
-    /** fetch the new set of repos based on new
-     *   filter/search data (queryData)
-     */
-    fetchRepos: function() {
-        var that = this;
-        $('#repo-list-container #loader').show();
-        $('#repo-list-container #repo-list').html('');
-        this.collection.fetch({
-            data: {
-                q: 'stars:' + this.queryData.get('stars').min + '..' + this.queryData.get('stars').max + ' language:' + this.queryData.get('language'),
-                sort: 'stars',
-                order: 'desc'
-            },
-            success: function(collection, response, options) {
-                // when we get response back we need to update
-                // X-RateLimit values
-
-                // siliently update this data
-                // we dont want to backbone to trigger change event for this.
-                // because change event is used for changes done by user
-                // using search autocomplete or start range
-                that.queryData.set({
-                    'XRateRemaining': options.xhr.getResponseHeader('X-RateLimit-Remaining'),
-                    'XRateLimit': options.xhr.getResponseHeader('X-RateLimit-Limit'),
-                    'totalRepos': response.total_count,
-                    'language': that.queryData.get('language')
-                }, {
-                    silent: true
-                });
-                // instead , we are emmiting update event here
-                that.queryData.trigger('update');
-            }
-        });
-    },
-    renderBody: function() {
+    render: function() {
         this.$el.html(_.template($('#Tpl-body').html()));
         $('#app-body-container').html(this.el);
-        this.activateAutocomplete();
-        this.renderFilter();
     },
-    /**
-     * filter is the side filter which has start range widget
-     * actually side filter has rendered twice
-     * # one for mobile view (which comes when user Swipes right)
-     * # other side filter is for devices tablets and above
-     */
-    renderFilter: function() {
-        $('.drag-target,#sidenav-overlay').remove();
-        $('#app-body-container .rightContent.hide-on-med-and-up').html(_.template($('#Tpl-filterMobile').html())(this.queryData.toJSON()));
-        $('#app-body-container .rightContent.hide-on-small-only').html(_.template($('#Tpl-filter').html())(this.queryData.toJSON()));
-        $('.button-collapse').sideNav({
-            menuWidth: 300,
-            edge: 'right',
-            closeOnClick: true
+    renderList: function() {
+        var userTpl = _.template($('#Tpl-user').html());
+        $('#user-list-container #loader').hide();
+        _.each(this.userList.models, function(model, key) {
+            $('#user-list-container #user-list').append(userTpl(model.toJSON()));
         });
-        this.activateSlider();
     },
-    // side filter has a slider (star Range) which we have to activate
-    activateSlider: function() {
+    findAndAddUser: function(e) {
         var that = this;
-        var sliders = $('.starSlider');
-        _.each(sliders, function(slider) {
-            noUiSlider.create(slider, {
-                start: [that.queryData.get('stars').min, that.queryData.get('stars').max],
-                connect: true,
-                step: 1,
-                range: {
-                    'min': 0,
-                    'max': 1000
-                },
-                format: wNumb({
-                    decimals: 0
-                })
-            });
-            slider.noUiSlider.on('set', function(e) {
-                that.updateSearchData(e);
-            });
+        this.userModel.userName = $('#autocomplete-input').val();
+        this.userModel.fetch({
+            success: function(model, response, options) {
+                console.log(model, response, that.userModel.userName);
+                that.userList.add(model);
+                that.userModel.save();
+            },
         });
-
-    },
-    // also activate the autocomplete search widget
-    activateAutocomplete: function() {
-        $.getJSON('https://rawgit.com/ashishsajwan/topgit-sap/gh-pages/data/languages.json', function(json) {
-            $('input.autocomplete').autocomplete({
-                data: json
-            });
-        });
-    },
-    // render Repo List
-    renderReposList: function() {
-        var listHtml = '';
-        var repoTemplate = _.template($('#Tpl-repo').html());
-        if (this.collection.total_count) {
-            _.each(this.collection.models, function(model, key) {
-                listHtml += repoTemplate(model.toJSON());
-            });
-        } else {
-            listHtml += '<div class="center-align">Oops.. couldn\'t find any such repositories</div>';
-        }
-        $('#repo-list-container #loader').hide();
-        $('#repo-list').html(listHtml);
-    },
-    // when clicked on repo it takes user to repo page in new tab
-    goToRepo: function(e) {
-        window.open($(e.currentTarget).attr('data-link'));
     }
-})
-
+});
 $(document).ready(function() {
-    var queryDataModel = new QueryDataModel();
 
     // create a object of Headerview
     // which will render header view of app
-    var headerView = new HeaderView({
-        model: queryDataModel
-    });
-
+    var headerView = new HeaderView();
     var bodyView = new BodyView({
-        queryData: queryDataModel,
-        // along with normal collection
-        collection: new RepoList()
+        userModel: new UserModel(),
+        userList: new UserList()
     });
 });
